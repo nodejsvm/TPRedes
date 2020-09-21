7afaa359d44b6e9cedcf14f945a0558d97adef2b
nodejsvm <nodejsvm@gmail.com>
2020-09-20 23:51:45 -0300

    nahuel estuvo aqui

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Api GeeSem lista." });
+  res.json({ message: "Api GeeSem lista nahuel estuvo aqui." });
 });
 
 // set port, listen for requests
